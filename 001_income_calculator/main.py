548eae5dbd94ca29abee04e566382661827f16a1
Shreyas <shreyas.global.7@gmail.com>
2026-07-28 13:47:03 +0530

    Update 001 project

--- 001_income_calculator/main.py
+++ 001_income_calculator/main.py
@@ -1,13 +1,37 @@
+# 1. This function calculates the finances.
+
 def calculate_finances(monthly_income: float,
                        tax_rate_in_percent: float,
                        currency_code: str,
                        expenses: dict[str, float] | None = None) -> None:
+
+    """
+    This function intends to calculate finance variables such as:
+
+    1-Monthly Net Income
+    2-Yearly Net Income
+    3-Yearly Gross Income
+    
+    It takes variables as follows:
+    
+    monthly_income: float,
+    tax_rate_in_percent: float,
+    currency_code: str,
+    expenses: dict[str, float] | None = None
+    
+    It also offers to calculate the users savings based on the provided expenses.
+    
+    """
+
+    # 2. Calculating the required variables.
 
     monthly_tax_amount = monthly_income * (tax_rate_in_percent / 100)
     monthly_net_income = monthly_income - monthly_tax_amount
     yearly_income = monthly_income * 12
     yearly_tax_amount = monthly_tax_amount * 12
     yearly_net_income = yearly_income - yearly_tax_amount
+
+    # 3. Displaying the calculated variables.
 
     print(f"{'-'*5} Finance Calculator {'-'*5}")
     print(f"Your Monthly Salary -> {currency_code} {monthly_income:.2f}")
@@ -33,6 +57,8 @@ def calculate_finances(monthly_income: float,
     for i, (label, value) in enumerate(zip(labels, values), start=1):
         print(f"{i} {label} {currency_code} {value:,.2f}")
 
+    # 4. If user wants to calculate savings, i.e savings is set to True (Default=None)
+
     if expenses:
         total_expenses = sum(expenses.values())
         monthly_savings = monthly_net_income - total_expenses
@@ -49,6 +75,7 @@ def calculate_finances(monthly_income: float,
             print("Warning: your expenses are greater than your monthly net income.")
     print(f"{'-'*30}")
 
+# 5.The main entry point of the script, which gathers the required info.
 def main():
     while True:
         try:
@@ -80,5 +107,6 @@ def main():
 
     calculate_finances(monthly_income, tax_rate_in_percent, currency_code, expenses)
 
+# 6. Runs the script.
 if __name__ == "__main__":
     main()
