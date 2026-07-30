ebfd58167db01bdd2a1414bfe4832ed4964ba3d7
Shreyas <shreyas.global.7@gmail.com>
2026-07-30 18:54:54 +0530

    Update 006 project

--- 006_currensy_converter/main.py
+++ 006_currensy_converter/main.py
@@ -2,30 +2,31 @@ import requests
 import json
 
 
-def get_country_codes()->list[dict[str, str]]|None:
+
+def get_country_codes()->list[dict[str, str]]:
+
     response = requests.get(url = fr"https://api.frankfurter.dev/v2/currencies")
     text_response: str = response.text
     json_object = json.loads(text_response)
     # print(json_object)
+
     return json_object
 
-    
+
 def fetch_currency_rate(base_currency: str,
                         date_: str, 
-                        quote_currency: str)->tuple[dict[str, str|None], dict[str, str|None]]|None:
+                        quote_currency: str)->tuple[dict[str, str|float], dict[str, str]]|None:
     
-    country_codes_list: list[dict[str, str]]|None = get_country_codes()
+    country_codes_list: list[dict[str, str]] = get_country_codes()
 
     try:
+
         response = requests.get(url = fr"https://api.frankfurter.dev/v2/rates?base={base_currency}&date={date_}")
         text_response: str = response.text
-        json_object = json.loads(text_response)
-        # print(json_object)
-
-        # country_codes_available: list[str] | None = [dct['quote'] for dct in json_object]
-        # print(len(country_codes_available))
+        json_object: list[dict[str, str|float]]= json.loads(text_response)
 
     except Exception as e:
+
         print(f'Error: {e}')
         print(country_codes_list)
         print(f'Please re-enter a valid base currency code from the above list')
@@ -33,26 +34,33 @@ def fetch_currency_rate(base_currency: str,
 
     else:
         for dct, dct2 in zip(json_object, country_codes_list):
+
             if quote_currency in dct.values():
+
                 print(f'Date: {dct['date']}')
                 print(f'Base Currency Code: {dct['base']}')
                 print(f'Target Currency Code: {dct['quote']}')
                 # print(f'Base Currency Price: {} 1/-')
                 print(f'Target Currency Price: {dct2['symbol']} {dct['rate']}/-')
+                
                 return (dct, dct2)
                 
             else:
                 pass
+
 
         print(f'{country_codes_list}')
         print(f'Please re-enter the details and use the quote currency code from the above given list.')
         main()
 
 
+
 def main()->None:
+
     base_currency: str = input('Please provide your base currency code >> ').upper()
     date_: str = input('Please provide the date [YYYY-MM-DD] of rate you want to check >> ')
     quote_currency: str = input('Please provide the currency you want to quote to >> ').upper()
+
     if base_currency != quote_currency:
         fetch_currency_rate(base_currency, date_, quote_currency)
     else:
